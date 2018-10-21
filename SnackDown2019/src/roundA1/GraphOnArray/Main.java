70af47872a69d01701efc0f303539841fabb7fda
Trung <trunghuynh.cse@gmail.com>
2018-10-21 11:12:05 +0800

    --

--- SnackDown2019/src/roundA1/GraphOnArray/Main.java
+++ SnackDown2019/src/roundA1/GraphOnArray/Main.java
@@ -15,48 +15,15 @@ public class Main {
         return a;
     }
 
-    static int __gcd(int a, int b) {
-        // Everything divides 0
-        if (a == 0 || b == 0) {
-            return 0;
-        }
-
-        // base case
-        if (a == b) {
-            return a;
-        }
-
-        // a is greater
-        if (a > b) {
-            return __gcd(a - b, b);
-        }
-
-        return __gcd(a, b - a);
-    }
-
-    static List<Integer> printDivisors(int n)
-    {
-        List<Integer> listOfDivisors= new ArrayList<>();
-        int maxD = (int)Math.sqrt(n);
-        for (int i=1; i<=maxD; i++)
-        {
-            if (n%i==0)
-            {
-                // If divisors are equal, print only one
-                if (n / i == i) {
-                    listOfDivisors.add(i);
-                }
-
-                // Otherwise print both
-                else {
-                    listOfDivisors.add(i);
-                    listOfDivisors.add(n / i);
-                }
+    private static Integer getPrime(Integer n){
+        Integer[] primes = {29, 31, 37, 41, 43, 47};
+        for(Integer i: primes){
+            if(n != i){
+                return i;
             }
         }
-        return listOfDivisors;
+        return 47;
     }
-
     /**
      * Ex:
      * tc1
@@ -70,8 +37,9 @@ public class Main {
      * 4 ->[]
      * 10 ->[]
      * 24 ->[]
+     *
      * tc3:
-     * 2 -> [5]
+     * 2 ->[5]
      * 5 ->[2, 8]
      * 8 ->[5]
      * 80 ->[]
@@ -79,8 +47,6 @@ public class Main {
     public static void processing(int size, Integer[] numbers) {
         int count = 0;
         StringBuilder tmp = new StringBuilder();
-
-
         Integer[] primes = {29, 31, 37, 41, 43, 47};
         int i = 0;
 
@@ -90,15 +56,9 @@ public class Main {
                 if (j == k) {
                     continue;
                 }
+
                 Integer a = numbers[j];
                 Integer b = numbers[k];
-
-//                List<Integer> l1 = printDivisors(a);
-//                List<Integer> l2 = printDivisors(b);
-//                l1.retainAll(l2);
-//                if(l1.size() == 1 && l1.get(0) == 1){
-//                    isCoPrime = true;
-//                }
 
                 if(gcd(a, b) == 1){
                     isCoPrime = true;
@@ -109,17 +69,16 @@ public class Main {
             //
             if (!isCoPrime) {
                 count++;
-                if (i >= 5) {
-                    i = 0;
-                }
-                numbers[j] = primes[i];
-                i++;
+                numbers[j] = getPrime(numbers[j]);
+
+
             }
 
 
             if(j > 0)
                 tmp.append(" ");
             tmp.append(numbers[j]);
+
 
         }
 
@@ -143,6 +102,8 @@ public class Main {
                 test.add(data);
                 numOfTestcases--;
             }
+
+
             for (Integer[] data : test) {
                 processing(data.length, data);
             }
